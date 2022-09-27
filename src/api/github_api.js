fb2e679f2f0673ecabbc3c8679a67a82d83515dc
Jhonatan1022780 <rojasjhonatan324@gmail.com>
2022-09-26 19:10:32 -0500

    add function

--- src/api/github_api.js
+++ src/api/github_api.js
@@ -1,29 +1,39 @@
-const api_key = import.meta.env.VITE_API_KEY;
+export default function handler(request, response) {
+    console.log("first log")
+    response.status(200).json({
+      body: request.body,
+      query: request.query,
+      cookies: request.cookies,
+    });
+  }
+  
 
-export const getUser = async (user) => {
-    const response = await fetch(
-        `https://api.github.com/users/${user}`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${api_key}`,
-            },
-        }
-    );
-    return await response.json();
-}
+// const api_key = import.meta.env.VITE_API_KEY;
 
-export const getCurrentUser = async () => {
-    const response = await fetch(
-        `https://api.github.com/user`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${api_key}`,
-            },
-        }
-    );
-    return await response.json();
-}
+// export const getUser = async (user) => {
+//     const response = await fetch(
+//         `https://api.github.com/users/${user}`,
+//         {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization: `Bearer ${api_key}`,
+//             },
+//         }
+//     );
+//     return await response.json();
+// }
+
+// export const getCurrentUser = async () => {
+//     const response = await fetch(
+//         `https://api.github.com/user`,
+//         {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization: `Bearer ${api_key}`,
+//             },
+//         }
+//     );
+//     return await response.json();
+// }
